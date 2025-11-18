89bebdea68142cf3e31b6db0d36b0c3ddcf2bf99
Eldor0614 <eravshanov48@gmail.com>
2025-11-18 18:35:59 +0500

    Update hw1.py

--- lesson-1/homework/hw1.py
+++ lesson-1/homework/hw1.py
@@ -1,1 +1,32 @@
+side=5
+p=side*4
+s=side**2
+print("Perimeter:", p)
+print("Area:", s)
 
+
+
+import math
+r  = int(input('Aylananing diameterini kirgizing:'))
+L = 2 * math.pi * r
+print('Aylana diametri:', L)
+
+
+
+a=int(input("Enter your first number:"))
+b=int(input("enter your second number:"))
+c=(a+b)/2
+print('The mean of', a, "and", b, "is",c)
+
+
+
+a=int(input("Enter your first number:"))
+b=int(input("enter your second number:"))
+c=a+b
+print('Total:', c)
+c=a*b
+print("The product of", a, "and", b, "is", c)
+c=a**2
+print("The square of", a, "is", c)
+c=b**2
+print("The square of", b, "is", c)
